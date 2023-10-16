3c8d7e40699822bd5201318d13eebb9edb3e10bb
Crowdbotics <team@crowdbotics.com>
2023-10-16 13:47:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sparkling-waterfall-43965",
+  url: "https://sparkling-waterfall-43965.botics.co",
+  api: "https://sparkling-waterfall-43965.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
